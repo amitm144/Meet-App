62c865fb31f3d5572d7664a3b271b779a7c6272b
Dvir.Tayeb <dvir.tayeb@s.afeka.ac.il>
2022-11-21 21:17:35 +0000

    Merged in feature/super-app-objects-api (pull request #6)
    Feature/super app objects api
    
    * added SuperApp Objects API
    
    * added createdBy boundary, modified objectsController, modified structure
    
    * added path variables to updateObject
    
    * added .gitignore file
    
    * modified objectController use the wrapper we created, deleted unnecessary file
    
    * remove print unnecessary, added email to exmaple object
    
    * modified get all object in objectController, added getNrandomObject func, fix issues
    
    
    Approved-by: Noam.Levy

---
+++ src/main/java/application/controllers/ObjectsController.java
@@ -0,0 +1,72 @@
+package application.controllers;
+
+import application.boundaries.command.ObjectIdBoundary;
+import application.boundaries.object.ObjectBoundary;
+import application.boundaries.user.UserIdBoundary;
+import application.util.wrappers.ObjectIdWrapper;
+import application.util.wrappers.UserIdWrapper;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static application.boundaries.object.ObjectBoundary.getNRandomObjects;
+
+@RestController
+public class ObjectsController {
+
+    @RequestMapping(
+            path = {"/superapp/objects"},
+            method = {RequestMethod.POST},
+            produces = {MediaType.APPLICATION_JSON_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    @ResponseBody
+    public ObjectBoundary createObject(@RequestBody ObjectBoundary objectBoundary) {
+        return objectBoundary;
+    }
+
+    @RequestMapping(
+            path = {"/superapp/objects/{miniapp}/{InternalObjectd}"},
+            method = {RequestMethod.PUT},
+            produces = {MediaType.APPLICATION_JSON_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    public void updateObject(ObjectBoundary objectBoundary,
+                             @PathVariable String miniapp,
+                             @PathVariable String InternalObjectd) {
+        //TODO - Update the specific object in DB with the miniapp and InternalObjectd vars
+    }
+
+    @RequestMapping(
+            path = {"/superapp/objects/{miniapp}/{InternalObjectd}"},
+            method = {RequestMethod.GET},
+            produces = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    public ObjectBoundary retrieveObject(@PathVariable String miniapp, @PathVariable String InternalObjectd) {
+        //TODO need to query from the DB one object from the miniapp paramter and InternalObjectd parameter.
+        Map<String, Object> tempMap = new HashMap<>();
+        tempMap.put("key", "temp");
+        tempMap.put("key2", "temp2");
+        // For Example, I created an object to show some data.
+        return new ObjectBoundary((new ObjectIdBoundary("id2")),
+                "example-type", "a", tempMap,
+                new UserIdWrapper(new UserIdBoundary("dvir.tayeb@gmail.com"))
+        );
+    }
+
+    @RequestMapping(
+            path = {"/superapp/objects"},
+            method = {RequestMethod.GET},
+            produces = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    @ResponseBody
+    public ArrayList<ObjectBoundary> getAllObjects() {
+        //TODO need to query from the DB to get all objects we want.
+
+        // For example, we returned some random array data.
+        return getNRandomObjects(3);
+    }
+}
