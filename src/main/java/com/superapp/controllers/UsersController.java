aeaac44807b54994ad6937ce6e7e467ed8c0c3b8
DaniellaVardi <105429946+DaniellaVardi@users.noreply.github.com>
2022-12-01 16:14:09 +0200

    fix bugs not finish

--- src/main/java/com/superapp/controllers/UsersController.java
+++ src/main/java/com/superapp/controllers/UsersController.java
@@ -12,7 +12,7 @@ public class UsersController {
             path= {"/superapp/users/login/{superapp}/{email}"},
             method = {RequestMethod.GET},
             produces = {MediaType.APPLICATION_JSON_VALUE})
-    public Object login (@PathVariable("superapp") String superapp, @PathVariable("email") String email) {
+    public UserBoundary login (@PathVariable("superapp") String superapp, @PathVariable("email") String email) {
         String username = email.split("@")[0];
         return new UserBoundary(superapp, email, "user", username, "A" );
     }
@@ -28,7 +28,6 @@ public class UsersController {
             path= {"/superapp/users/{superapp}/{userEmail}"},
             method = {RequestMethod.PUT},
             consumes = {MediaType.APPLICATION_JSON_VALUE})
-
     public void update (@PathVariable("superapp") String superapp , @PathVariable("userEmail") String email) { }
 
 }
