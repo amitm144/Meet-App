797c9e2488f09780ba9fbc0ed00d9b9a0266fb9d
Dvir Tayeb <dvirtayeb@Dvirs-MacBook-Pro.local>
2022-12-13 18:20:18 +0200

    remove the unnecessary comments from test

--- src/test/java/application/DemoControllerTests.java
+++ src/test/java/application/DemoControllerTests.java
@@ -1,52 +1,52 @@
-//package application;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//import javax.annotation.PostConstruct;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-//import org.springframework.boot.test.web.server.LocalServerPort;
-//import org.springframework.web.client.RestTemplate;
-//
-//@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-//public class DemoControllerTests {
-//	private int port;
-//	private RestTemplate restTemplate;
-//	private String url;
-//
-//	@LocalServerPort
-//	public void setPort(int port) {
-//		this.port = port;
-//	}
-//
-//	@PostConstruct
-//	public void init() {
-//		this.restTemplate = new RestTemplate();
-//		this.url = "http://localhost:" + this.port + "/application";
-//		System.out.println(this.url);
+package application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.annotation.PostConstruct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class DemoControllerTests {
+	private int port;
+	private RestTemplate restTemplate;
+	private String url;
+
+	@LocalServerPort
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	@PostConstruct
+	public void init() {
+		this.restTemplate = new RestTemplate();
+		this.url = "http://localhost:" + this.port + "/application";
+		System.out.println(this.url);
+	}
+
+	@Test
+	public void testContext() throws Exception {
+	}
+
+//	@Test
+//	public void testHelloWithValidName () throws Exception {
+//		assertThat(
+//		  this.restTemplate
+//			.getForObject(this.url + "/{name}", DemoBoundary.class, "test"))
+//			.usingRecursiveComparison()
+//			.isEqualTo(new DemoBoundary("Hello test"));
 //	}
 //
 //	@Test
-//	public void testContext() throws Exception {
+//	public void testHelloWithEmptyName () throws Exception {
+//		assertThat(
+//		  this.restTemplate
+//			.getForObject(this.url + "/{name}", DemoBoundary.class, "   "))
+//			.usingRecursiveComparison()
+//			.isEqualTo(new DemoBoundary("Hello Anonymous"));
 //	}
-//
-////	@Test
-////	public void testHelloWithValidName () throws Exception {
-////		assertThat(
-////		  this.restTemplate
-////			.getForObject(this.url + "/{name}", DemoBoundary.class, "test"))
-////			.usingRecursiveComparison()
-////			.isEqualTo(new DemoBoundary("Hello test"));
-////	}
-////
-////	@Test
-////	public void testHelloWithEmptyName () throws Exception {
-////		assertThat(
-////		  this.restTemplate
-////			.getForObject(this.url + "/{name}", DemoBoundary.class, "   "))
-////			.usingRecursiveComparison()
-////			.isEqualTo(new DemoBoundary("Hello Anonymous"));
-////	}
-//}
+}
