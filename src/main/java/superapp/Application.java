e1255eac6ab025cd7b89674651eeea6b9c03f464
Noam Levy <noam5003@gmail.com>
2022-12-29 20:54:48 +0200

    added multiple properties files and new staging db

--- src/main/java/superapp/Application.java
+++ src/main/java/superapp/Application.java
@@ -3,9 +3,23 @@ package superapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Properties;
+
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication application = new SpringApplication(Application.class);
+		Properties defaultProperties = new Properties();
+
+		/* TODO: activate on production environment */
+		defaultProperties.setProperty("spring.profiles.active", "production");
+		/* TODO: activate on stageing environment (if you want to test against MySql test db) */
+//		defaultProperties.setProperty("spring.profiles.active", "staging");
+		/* TODO: activate on production environment (if you want to test with h2 locally) */
+//		defaultProperties.setProperty("spring.profiles.active", "test");
+
+		defaultProperties.setProperty("spring.config.name", "application-${spring.profiles.active}");
+		application.setDefaultProperties(defaultProperties);
+		application.run(args);
 	}
 }
