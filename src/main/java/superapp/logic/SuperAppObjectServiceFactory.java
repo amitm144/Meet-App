aaa6bb77d93ef525feb4717a77e8f5efd89a0b3b
YuvalM <yuvalsmastey@gmail.com>
2022-12-29 16:26:39 +0200

    deleted old files

--- src/main/java/superapp/logic/SuperAppObjectServiceFactory.java
+++
@@ -1,9 +0,0 @@
-package superapp.logic;
-
-import superapp.boundaries.object.SuperAppObjectBoundary;
-import superapp.data.SuperAppObjectEntity;
-
-public interface SuperAppObjectFactory {
-   public SuperAppObjectBoundary setObjectDetails(SuperAppObjectBoundary object);
-   public SuperAppObjectEntity updateObjectDetails(SuperAppObjectEntity object);
-}
