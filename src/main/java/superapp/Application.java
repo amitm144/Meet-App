d4263654b0fab5568b91a7e57b4376ec540d42b3
Noam Levy <noam5003@gmail.com>
2022-12-30 11:13:31 +0200

    properties abstraction to environment variable

--- src/main/java/superapp/Application.java
+++ src/main/java/superapp/Application.java
@@ -1,5 +1,6 @@
 package superapp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,14 +11,23 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication application = new SpringApplication(Application.class);
 		Properties defaultProperties = new Properties();
-
-		/* TODO: activate on production environment */
-		defaultProperties.setProperty("spring.profiles.active", "production");
-		/* TODO: activate on stageing environment (if you want to test against MySql test db) */
-//		defaultProperties.setProperty("spring.profiles.active", "staging");
-		/* TODO: activate on production environment (if you want to test with h2 locally) */
-//		defaultProperties.setProperty("spring.profiles.active", "test");
-
+		/*  INSTRUCTIONS:
+		    add and or change ACTIVE_PROFILE environment variable to choose the correct properties.
+		    the various property files will determine the application behaviour:
+		    1. production - HelperInitializer will be DISABLED and JPA will connect to the PRODUCTION db
+		    	changes you will make WILL NOT be deleted from the database when you shut down the server.
+		    2. staging - HelperInitializer will be ENABLED and JPA will connect to the STAGING db
+		    	this setting is appropriate for testing in similar environment to production
+		    	changes you will make WILL be deleted from the database when you shut down the server.
+		    3. test - HelperInitializer will be DISABLED and JPA will connect to H2,
+		    	this setting is appropriate for unit testing
+		    	or if you wish to play with the app locally with no effect on any database
+		    BY DEFAULT THE APPLICATION WILL START WITH STAGING PROPERTIES APPLIED
+		*/
+		String activeProfile = System.getenv("ACTIVE_PROFILE");
+		if (activeProfile == null || activeProfile.isBlank())
+			activeProfile = "staging";
+		defaultProperties.setProperty("spring.profiles.active", activeProfile);
 		defaultProperties.setProperty("spring.config.name", "application-${spring.profiles.active}");
 		application.setDefaultProperties(defaultProperties);
 		application.run(args);
