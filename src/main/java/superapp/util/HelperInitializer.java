e31ff078b1cf1ac9c2f608a1d1587f28bf3f214d
amitm144 <87026850+amitm144@users.noreply.github.com>
2023-01-03 14:02:04 +0200

    make intstram to hekper initializer

--- src/main/java/superapp/util/HelperInitializer.java
+++ src/main/java/superapp/util/HelperInitializer.java
@@ -15,7 +15,9 @@ import superapp.logic.concreteServices.UserService;
 import superapp.util.wrappers.UserIdWrapper;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 @Component // TODO initialize this object ONLY when testing manually the server
 public class HelperInitializer implements CommandLineRunner {
@@ -34,49 +36,51 @@ public class HelperInitializer implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		//user
-		UserIdBoundary userId1 =
-				new UserIdBoundary(this.users.getSuperappName() ,"user1@test.com" );
-		UserIdBoundary userId2 =
-				new UserIdBoundary(this.users.getSuperappName() ,"user2@test.com" );
 
-		UserIdWrapper userIdWrapper1 =
-				new UserIdWrapper(userId1);
-		UserIdWrapper userIdWrapper2 =
-				new UserIdWrapper(userId2);
+		List<UserBoundary> usersList =
+				IntStream
+						.range(0, 2)
+						.mapToObj(i->
+								this.users.createUser(new UserBoundary(
+										new UserIdBoundary(
+												users.getSuperappName(),
+												"user"+i+"@test.com"),
+										"SUPERAPP_USER",
+										""+i,
+										"a")))
+						.toList();
 
-		UserBoundary user1 =
-				this.users.createUser(new UserBoundary(userId1,"SUPERAPP_USER","user1","avatar1"));
-		UserBoundary user2 =
-				this.users.createUser(new UserBoundary(userId2,"SUPERAPP_USER","user2","avatar2"));
+		//object group
 
-		//object
-		Map<String, Object> objectDetails = new HashMap<String, Object>();
-		objectDetails.put("Object attribute1",1);
-		objectDetails.put("Object attribute2",2);
+		List<SuperAppObjectBoundary> groupList =
+				IntStream
+						.range(0, 2)
+						.mapToObj(i->
+								this.objects.createObject(new SuperAppObjectBoundary(
+										new SuperAppObjectIdBoundary(),
+										"Group"+i,
+										"a",
+										new HashMap<String, Object>(){{put("members",usersList);}},
+										new UserIdWrapper(usersList.get(0).getUserId()))))
 
-		SuperAppObjectIdBoundary objId1 =
-				new SuperAppObjectIdBoundary(user1.getUserId().getSuperapp(), "11");
-		SuperAppObjectIdBoundary objId2 =
-				new SuperAppObjectIdBoundary(user2.getUserId().getSuperapp(), "12");
+						.toList();
 
-		SuperAppObjectBoundary obj1 =
-				this.objects.createObject(new SuperAppObjectBoundary(objId1,"object1","alias1",objectDetails,userIdWrapper1));
-		SuperAppObjectBoundary obj2 =
-				this.objects.createObject(new SuperAppObjectBoundary(objId2,"object2","alias2",objectDetails,userIdWrapper2));
+//		//command
 
-		//command
-		MiniAppCommandIdBoundary commandId1 =
-				new MiniAppCommandIdBoundary("miniApp1","21");
-		MiniAppCommandIdBoundary commandId2 =
-				new MiniAppCommandIdBoundary("miniApp2","22");
-
-		Map<String, Object> commandDetails = new HashMap<String, Object>();
-		commandDetails.put("command attribute1",1);
-		commandDetails.put("command attribute2",2);
-
-		MiniAppCommandBoundary command1 =
-				(MiniAppCommandBoundary) this.commands.invokeCommand(new MiniAppCommandBoundary(commandId1,"test1",objId1,userId1,commandDetails));
-		MiniAppCommandBoundary command2 =
-				(MiniAppCommandBoundary) this.commands.invokeCommand(new MiniAppCommandBoundary(commandId2,"test2",objId2,userId2,commandDetails));
+		List<Object> commandsList =
+				IntStream
+						.range(0, 2)
+						.mapToObj(i->
+								this.commands.invokeCommand(new MiniAppCommandBoundary(
+										new MiniAppCommandIdBoundary(
+												"Split",
+												""+i
+										),
+										"showDebt",
+										groupList.get(0).getObjectId(),
+										usersList.get(0).getUserId(),
+										new HashMap<String, Object>()
+								)
+								)).toList();
 	}
 }
