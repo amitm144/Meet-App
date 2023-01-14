132e395af844b6db11a83d7a6887403cb0482278
Dvir Tayeb <dvirtayeb@Dvirs-MacBook-Pro.local>
2023-01-14 17:12:06 +0200

    added logs for user service

--- src/main/java/superapp/logic/concreteServices/UserService.java
+++ src/main/java/superapp/logic/concreteServices/UserService.java
@@ -1,6 +1,7 @@
 package superapp.logic.concreteServices;
 
-import superapp.boundaries.user.NewUserBoundary;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import superapp.boundaries.user.UserIdBoundary;
 import superapp.converters.UserConverter;
 import superapp.dal.UserEntityRepository;
@@ -29,6 +30,7 @@ import static superapp.util.Constants.*;
 public class UserService extends AbstractService implements AdvancedUsersService {
     private UserEntityRepository userEntityRepository;
     private UserConverter converter;
+    private Log logger = LogFactory.getLog(UserService.class);
 
     @Autowired
     public UserService(UserConverter converter,
@@ -45,14 +47,17 @@ public class UserService extends AbstractService implements AdvancedUsersService
                 !EmailChecker.isValidEmail(userId.getEmail()) ||
                 user.getAvatar() == null || user.getUsername() == null ||
                 user.getAvatar().isBlank() ||  user.getUsername().isBlank() ||
-                !UserRole.isValidRole(user.getRole()))
+                !UserRole.isValidRole(user.getRole())) {
+            this.logger.error("Invalid User details");
             throw new InvalidInputException("Invalid User details");
+        }
 
         user.setSuperApp(this.superappName);
         Optional<UserEntity> userE = this.userEntityRepository.findById(new UserPK(userId.getSuperapp(), userId.getEmail()));
-        if (userE.isPresent())
+        if (userE.isPresent()) {
+            this.logger.error("In createUser func - User already exists");
             throw new AlreadyExistsException("User already exists");
-
+        }
         this.userEntityRepository.save(this.converter.toEntity(user));
         return user;
     }
@@ -60,52 +65,66 @@ public class UserService extends AbstractService implements AdvancedUsersService
     @Override
     @Transactional(readOnly = true)
     public UserBoundary login(String superapp, String userEmail) {
-        if (!isValidSuperapp(superapp))
+        if (!isValidSuperapp(superapp)) {
+            this.logger.error("in login func - Invalid super App");
             throw new InvalidInputException("Invalid superapp");
 
+        }
         Optional<UserEntity> user = this.userEntityRepository.findById(new UserPK(superapp, userEmail));
-        if (user.isEmpty())
+        if (user.isEmpty()) {
+            this.logger.error("in login func - Unknown user");
             throw new NotFoundException("Unknown user");
-
+        }
+        this.logger.info("User Created successfully");
         return this.converter.toBoundary(user.get());
     }
 
     @Override
     @Transactional
     public UserBoundary updateUser(String superapp, String userEmail, UserBoundary update) {
-        if (!isValidSuperapp(superapp))
+        if (!isValidSuperapp(superapp)) {
+            this.logger.error("in updateUser func - Invalid super App");
             throw new InvalidInputException("Invalid superapp");
-
+        }
         Optional<UserEntity> userOpt = this.userEntityRepository.findById(new UserPK(superapp, userEmail));
-        if (userOpt.isEmpty())
+        if (userOpt.isEmpty()) {
+            this.logger.error("in updateUser func - Unknown User");
             throw new NotFoundException("Unknown user");
-
+        }
         UserEntity user = userOpt.get();
-        if (!user.getSuperapp().equals(this.superappName))
+        if (!user.getSuperapp().equals(this.superappName)) {
+            this.logger.error("in updateUser func - Wrong super app");
             throw new CannotProcessException("Cannot update this user - Wrong superapp");
-
+        }
         String newUserName = update.getUsername();
         String newAvatar = update.getAvatar();
         String newRole = update.getRole();
 
         if (newUserName != null)
-            if (newUserName.isBlank())
+            if (newUserName.isBlank()) {
+                this.logger.error("in updateUser func - Invalid username");
                 throw new InvalidInputException("Invalid username");
+            }
             else
                 user.setUsername(newUserName);
 
         if (newAvatar != null)
-            if (newAvatar.isBlank())
+            if (newAvatar.isBlank()) {
+                this.logger.error("in updateUser func - Invalid username");
                 throw new InvalidInputException("Invalid user avatar");
+            }
             else
                 user.setAvatar(newAvatar);
 
         if (newRole != null) {
             try {
                 user.setRole(UserRole.valueOf(newRole));
-            } catch (IllegalArgumentException e) { throw new InvalidInputException("Illegal user role"); }
+            } catch (IllegalArgumentException e) {
+                this.logger.error("in updateUser func - Illegal user role");
+                throw new InvalidInputException("Illegal user role"); }
         }
         userEntityRepository.save(user);
+        this.logger.info("User Updated successfully");
         return this.converter.toBoundary(user);
     }
 
@@ -113,6 +132,7 @@ public class UserService extends AbstractService implements AdvancedUsersService
     @Deprecated
     @Transactional(readOnly = true)
     public List<UserBoundary> getAllUsers() {
+        this.logger.error("in getAllUsers func - calling to - %s".formatted(DEPRECATED_EXCEPTION));
         throw new InvalidInputException(DEPRECATED_EXCEPTION);
     }
 
@@ -120,8 +140,10 @@ public class UserService extends AbstractService implements AdvancedUsersService
     @Transactional(readOnly = true)
     public List<UserBoundary> getAllUsers(String userSuperapp, String email,int size,int page) {
         UserPK userId = new UserPK(userSuperapp, email);
-        if (!this.isValidUserCredentials(userId, ADMIN, this.userEntityRepository))
+        if (!this.isValidUserCredentials(userId, ADMIN, this.userEntityRepository)) {
+            this.logger.error("in getAllUsers func - %s".formatted(ADMIN_ONLY_EXCEPTION));
             throw new ForbbidenOperationException(ADMIN_ONLY_EXCEPTION);
+        }
 
         Iterable<UserEntity> users = this.userEntityRepository
                 .findAll(PageRequest.of(page,size, DEFAULT_SORTING_DIRECTION,"superapp","email"));
@@ -135,6 +157,7 @@ public class UserService extends AbstractService implements AdvancedUsersService
     @Deprecated
     @Transactional
     public void deleteAllUsers() {
+        this.logger.error("in deleteAllUsers func - calling to - %s".formatted(DEPRECATED_EXCEPTION));
         throw new InvalidInputException(DEPRECATED_EXCEPTION);
     }
 
@@ -142,9 +165,12 @@ public class UserService extends AbstractService implements AdvancedUsersService
     @Transactional
     public void deleteAllUsers(String userSuperapp, String email) {
         UserPK userId = new UserPK(userSuperapp, email);
-        if (!this.isValidUserCredentials(userId, ADMIN, this.userEntityRepository))
+        if (!this.isValidUserCredentials(userId, ADMIN, this.userEntityRepository)) {
+            this.logger.error("in deleteAllUsers func - %s".formatted(ADMIN_ONLY_EXCEPTION));
             throw new ForbbidenOperationException(ADMIN_ONLY_EXCEPTION);
+        }
 
         this.userEntityRepository.deleteAll();
+        this.logger.info("All Users deleted successfully");
     }
 }
