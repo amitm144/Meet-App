5f5950518b74fd8b40b8f902ad4944892fef3ecd
Noam Levy <noam5003@gmail.com>
2023-01-19 11:34:59 +0200

    fixed imports and unuesd variables

--- src/main/java/superapp/util/HelperInitializer.java
+++ src/main/java/superapp/util/HelperInitializer.java
@@ -5,34 +5,21 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import superapp.boundaries.command.MiniAppCommandBoundary;
-import superapp.boundaries.command.MiniAppCommandIdBoundary;
-import superapp.boundaries.object.SuperAppObjectBoundary;
 import superapp.boundaries.user.UserBoundary;
-import superapp.logic.concreteServices.MiniAppCommandService;
-import superapp.logic.concreteServices.SuperAppObjectService;
 import superapp.logic.concreteServices.UserService;
-import superapp.util.wrappers.UserIdWrapper;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.StreamSupport;
 
 @Component
 @Profile("staging")
 public class HelperInitializer implements CommandLineRunner {
-	private MiniAppCommandService commands;
-	private SuperAppObjectService objects;
 	private UserService users;
 
 	@Autowired
-	public HelperInitializer(MiniAppCommandService commands, SuperAppObjectService objects, UserService users) {
+	public HelperInitializer(UserService users) {
 		super();
-		this.commands = commands;
-		this.objects = objects;
 		this.users = users;
 	}
 
